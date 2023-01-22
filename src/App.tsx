04a0651cc006de03d0f634580b8be031960a1861
Walisson Silva <walissonsilva.dev@gmail.com>
2023-01-22 01:06:23 -0300

    feat: create project and configure tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="bg-gray-900 h-screen w-full flex items-center justify-center">
+      <h1 className="text-2xl text-emerald-500">Habits Application</h1>
+    </main>
+  );
+}
+
+export default App;
